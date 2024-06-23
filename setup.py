f132720385315fa37f251b9b3b57f36b4441d79b
Shivendra <shivharsh44@gmail.com>
2024-06-23 13:28:37 +0530

    making a package release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='axon',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='A lightweight array manipulation library written in python',
+    author='Shivendra',
+    author_email='shivharsh44@gmail.com',
+    url='https://github.com/shivendrra/axon',
+)
